1d2a866d96d519b224af00820694e1d3005617f6
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2026-03-02 15:08:21 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- debug/silu_ones_state_bug/exp1.py
+++ debug/silu_ones_state_bug/exp1.py
@@ -1,4 +1,4 @@
 import torch
 
-d = torch.empty(3, device="spyre", dtype=torch.float16)
+d = torch.tensor([1, 2, 3], dtype=torch.float16, device="spyre")
 print(torch.nn.functional.silu(d))
--- debug/silu_ones_state_bug/exp2.py
+++ debug/silu_ones_state_bug/exp2.py
@@ -1,3 +1,7 @@
 import torch
 
-print(torch.nn.functional.silu(torch.empty(3, device="spyre", dtype=torch.float16)))
+print(
+    torch.nn.functional.silu(
+        torch.tensor([1, 2, 3], dtype=torch.float16, device="spyre")
+    )
+)
